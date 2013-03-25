eb75b01f43d1e4253b280e78261d0b371e1870f9
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-03-25 00:11:56 -0500

    Commit of collection instantiation and tests.

---
+++ collectionsts.ts
@@ -0,0 +1,78 @@
+﻿
+///#region Interfaces
+export interface ICollection {
+    forEach(op: Function): void;
+    toArray(): any[];
+    add(elem: any): void;
+    remove(elem: IUnique): void;
+    removeById(id: string): void;
+    get (id: string): void;
+    length: number;
+}
+
+export interface IUnique {
+    uid: string;
+}
+
+interface UniqueHash {
+    [id: string]: IUnique;
+}
+///#endregion
+
+/// Based on: https://github.com/montagejs/collections/blob/master/shim-object.js#L73
+function generateHash(): string {
+    return Math.random().toString(36).slice(2);
+}
+
+export class Collection implements ICollection {
+    private _items = <UniqueHash>{};
+
+    private _length = 0;
+    public get length(): number { return this._length; }
+
+    public get (id: string): IUnique {
+        if (!this._items[id]) return null;
+        else return this._items[id];
+    }
+
+    private getHash() {
+        var hash = generateHash();
+        while (this._items[hash] !== undefined) hash = generateHash();
+        return hash;
+    }
+
+    public add(elem: any): void {
+        if (!elem.uid) {
+            elem.uid = this.getHash();
+        }
+        this._items[elem.uid] = elem;
+        this._length++;
+    }
+
+    public remove(elem: IUnique): void {
+        this.removeById(elem.uid);
+    }
+
+    public removeById(id: string): void {
+        this._items[id] = undefined;
+        this._length--;
+    }
+
+    public forEach(op: (elem: IUnique, id: string, i: number, list: Collection) => void ) {
+        var i = 0;
+        for (var id in this._items) {
+            op(this._items[id], id, i++, this);
+        }
+    }
+
+    public toArray(): IUnique[] {
+        var arr: IUnique[] = new Array(this.length);
+        this.forEach((elem, id, i) => { arr[i] = elem; });
+        return arr;
+    }
+}
+
+export function toUnique(obj: any, id: string): IUnique {
+    obj.uid = id;
+    return <IUnique> obj;
+}
---
+++ test/test.ts
@@ -0,0 +1,38 @@
+﻿/// <reference path="../collectionsts.ts" />
+/// <reference path="node.d.ts" />
+
+import assert = module("assert");
+import c = module("../collectionsts");
+
+var Collection = c.Collection;
+
+declare var describe: (descriptor: string, suite: () => void ) => void;
+declare var it: (descriptor: string, test: (done: (err?: any) => void ) => void ) => void;
+
+var id = 0;
+class Uniq implements c.IUnique {
+    public uid: string = String(id++);
+}
+
+describe("Collection", function () {
+    it("should contain three items", function () {
+        var col = new Collection();
+        for(var i = 0; i < 3; i++)
+            col.add(new Uniq());
+        assert(col.length === 3);
+    });
+
+    it("should count 100,000 items", function () {
+        var col = new Collection();
+        for (var i = 0; i < 1e5; i++) {
+            col.add({ i: i });
+        }
+        
+        var j = 0;
+        col.forEach(function (elem, id, i) {
+            j = i;
+        });
+
+        assert.equal(j, 99999);
+    });
+});
